3922836aad18bbf57a733a4983bce4d43ea4f0b8
Osipov Roman <r.osipov@rambler-co.ru>
2023-09-22 10:12:02 +0300

    Доработана реализация функций в handlers в качестве методов структуры handler

--- internal/api/api.go
+++ internal/api/api.go
@@ -10,22 +10,22 @@ import (
 )
 
 type APIServer struct {
-	storage *storage.MemStorage
-	echo    *echo.Echo
-	addr    string
+	echo *echo.Echo
+	addr string
 }
 
 func New() *APIServer {
 	apiS := &APIServer{}
-	apiS.storage = storage.New()
 	apiS.echo = echo.New()
 	cfg := config.ServerConfig{}
+	st := storage.New()
+	handler := handlers.New(st)
 	cfg.New()
 	apiS.addr = cfg.Addr
 
-	apiS.echo.GET("/", handlers.AllMetricsValues(apiS.storage))
-	apiS.echo.GET("/value/:typeM/:nameM", handlers.MetricsValue(apiS.storage))
-	apiS.echo.POST("/update/:typeM/:nameM/:valueM", handlers.PostWebhandle(apiS.storage))
+	apiS.echo.GET("/", handler.AllMetricsValues())
+	apiS.echo.GET("/value/:typeM/:nameM", handler.MetricsValue())
+	apiS.echo.POST("/update/:typeM/:nameM/:valueM", handler.PostWebhandle())
 
 	return apiS
 }
