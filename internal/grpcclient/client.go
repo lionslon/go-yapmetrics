164568ac8a11a8f42b714c3a4ab40caa690d1b17
Roman Osipov <r.osipov@MAC2327.local>
2024-12-24 22:25:35 +0300

    Fix statictest#3

--- internal/grpcclient/client.go
+++ internal/grpcclient/client.go
@@ -1,42 +1,45 @@
 package grpcclient
 
 import (
-    "context"
-    "time"
+	"context"
+	"google.golang.org/grpc/credentials/insecure"
+	"time"
 
-    pb "github.com/lionslon/go-yapmetrics/api"
-    "google.golang.org/grpc"
+	pb "github.com/lionslon/go-yapmetrics/api"
+	"google.golang.org/grpc"
 )
 
 type Client struct {
-    conn   *grpc.ClientConn
-    client pb.MetricsServiceClient
+	conn   *grpc.ClientConn
+	client pb.MetricsServiceClient
 }
 
 func NewClient(addr string) (*Client, error) {
-    conn, err := grpc.Dial(addr, grpc.WithInsecure())
-    if err != nil {
-        return nil, err
-    }
-    return &Client{
-        conn:   conn,
-        client: pb.NewMetricsServiceClient(conn),
-    }, nil
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		conn:   conn,
+		client: pb.NewMetricsServiceClient(conn),
+	}, nil
 }
 
 func (c *Client) SendMetric(metric *pb.Metric) error {
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    _, err := c.client.SendMetric(ctx, metric)
-    return err
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	_, err := c.client.SendMetric(ctx, metric)
+	return err
 }
 
 func (c *Client) GetMetrics() (*pb.MetricsList, error) {
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    return c.client.GetMetrics(ctx, &pb.Empty{})
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	return c.client.GetMetrics(ctx, &pb.Empty{})
 }
 
 func (c *Client) Close() {
-    c.conn.Close()
+	c.conn.Close()
 }
